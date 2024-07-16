3b684f6887997981c9032cdc24852cdf6a642f7c
Aktan <aktan.abykeev@gmail.com>
2024-07-16 10:25:26 +0300

    update user info is done

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -18,44 +18,6 @@ class ProfileController extends Controller
     }
 
 
-    // public function updateUserInfo(Request $request, $id) {
-    //     $user = User::find($id);
-
-    //     if (!$user) {
-    //         return redirect()->route('/')->with('error', 'User not found.');
-    //     }
-
-    //     $incomingFields = $request->validate([
-    //         'name' => 'required',
-    //         'surname' => 'required',
-    //         'email' => ['required', 'email', Rule::unique('users')->ignore($user->id)],
-    //         'grade_level' => 'required',
-    //         'number' => 'required',
-    //         'school' => 'required',
-    //         'location' => 'required',
-    //         'current_password' => 'nullable',
-    //         'password' => 'sometimes|nullable|confirmed'
-    //     ]);
-
-    //     if (!empty($incomingFields['current_password'])) {
-    //         if (Hash::check($incomingFields['current_password'], $user->password)) {
-    //             if (!empty($incomingFields['password'])) {
-    //                 $incomingFields['password'] = bcrypt($incomingFields['password']);
-    //             } else {
-    //                 unset($incomingFields['current_password']);
-    //             }
-    //         } else {
-    //             return redirect()->back()->with('error', 'Current password is incorrect.');
-    //         }
-    //     } else {
-    //         unset($incomingFields['password']);
-    //     }
-
-    //     $user->update($incomingFields);
-
-    //     return redirect()->route('', ['encryptedId' => encrypt($user->id)])
-    //                      ->with('success', 'Profile updated successfully.');
-    // }
     public function updateUserInfo(Request $request, $id) {
         $user = User::find($id);
 
